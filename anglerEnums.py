2fcd6e43893a16d9cad202c6356f75ee8a05bfea
Simon Deutsch <>
2022-02-15 21:13:48 +0100

    manual & auto mode switch

---
+++ anglerEnums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class AnglerConfig(Enum):
+    GENERATIONFOLDER = "generationFolder"
+    GATEWAY = "swaggerUI"
+    MODE = "mode" # detect (default) / manual ()
+    DEFINITIONS = "definitions" #only read if mode = manual
+    OPENAPICLIADD = "openapi-cli-add" # additional props for openapi gen
+
+class AnglerMode(Enum):
+    AUTO = "auto"
+    MANUAL = "manual"
